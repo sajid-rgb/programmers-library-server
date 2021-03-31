d2ce05f23eae98d3cf516238723abcee6cca2f98
Sajid Chowdhury <msrchk@gmail.com>
2021-03-31 07:28:44 +0600

    server Full complete

---
+++ README.md
@@ -0,0 +1,1 @@
+# full-stack-server-sajid-rgb
---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const port = 5000;
+const cors = require('cors')
+const bodyParser = require('body-parser');
+const app = express();
+const dotenv = require('dotenv')
+dotenv.config()
+app.use(cors())
+app.use(bodyParser.json())
+app.get('/',(req,res)=>{
+    res.send('Welcome')
+})
+console.log(process.env.DB_USER);
+const MongoClient = require('mongodb').MongoClient;
+const  ObjectID= require('mongodb').ObjectID;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.weqbi.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const booksCollection = client.db("booksstore").collection("book");
+  app.post('/addBooks',(req,res)=>{
+      console.log(req.body);
+      booksCollection.insertOne(req.body)
+
+  })
+  app.get('/books',(req,res)=>{
+      booksCollection.find({})
+      .toArray((err,documents)=>{
+          res.send(documents)
+      })
+  })
+  app.get('/books/:id',(req,res)=>{
+    booksCollection.find({_id:ObjectID(req.params.id)})
+    .toArray((err,documents)=>{
+        res.send(documents)
+    })
+})
+app.delete('/deleteBooks/:id',(req,res)=>{
+    booksCollection.deleteOne({_id:ObjectID(req.params.id)})
+})
+   console.log('connected');
+});
+
+app.listen(process.env.PORT || port)
